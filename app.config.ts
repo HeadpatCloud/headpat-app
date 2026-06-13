e9071f6ac727cd3d3e98f22cd1a43dd851b463a1
Faye <faye@fayevr.dev>
2026-06-13 19:23:14 +0200

    chore: bump version

--- app.config.ts
+++ app.config.ts
@@ -14,7 +14,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
 	...config,
 	name: "Headpat",
 	slug: "headpat-app",
-	version: "0.9.1",
+	version: "0.9.2",
 	orientation: "portrait",
 	icon: "./assets/images/icon.png",
 	scheme: "headpat",
